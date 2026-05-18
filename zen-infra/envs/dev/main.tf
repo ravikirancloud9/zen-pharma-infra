508b4bdb43c36c4acbdeccba90e435cb0069a7c8
ravikirancloud9 <ravikiran.cloud9@gmail.com>
2026-05-18 17:01:43 -0400

    Added EKS

--- zen-infra/envs/dev/main.tf
+++ zen-infra/envs/dev/main.tf
@@ -15,3 +15,16 @@ module "vpc" {
   private_eks_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
   private_rds_subnet_cidrs = ["10.0.5.0/24", "10.0.6.0/24"]
 }
+
+module "eks" {
+  source = "../../modules/aws_eks"
+
+  project            = "pharma"
+  env                = "dev"
+  cluster_version    = "1.33"
+  subnet_ids         = module.vpc.private_eks_subnet_ids
+  node_instance_type = "t3.small"
+  desired_capacity   = 2
+  min_size           = 1
+  max_size           = 4
+}
